3d5e83cf6891d4e027ab49e88025907596757ac7
Gideon Dresdner <gideond@gmail.com>
2013-11-19 16:08:40 -0500

    use finite state machines

--- remove-comments.c
+++ remove-comments.c
@@ -5,23 +5,28 @@
 
 int getline(char s[]);
 
+void double_slash();
+void code();
+void slash(char prev);
+
 int main(int argc, char **argv)
 {
-    int state;
-    int DOUBLE_SLASH = 1;
-    int SLASH_STAR = 2;
-    char b[MAXLINE];
+    code();
+    //int state;
+    //int DOUBLE_SLASH = 1;
+    //int SLASH_STAR = 2;
+    //char b[MAXLINE];
 
-    while (0 != (getline(b))) {
-        state = 0;
+    //while (0 != (getline(b))) {
+    //    state = 0;
 
-        if ('/' == b[0] && '/' == b[1])
-            state = DOUBLE_SLASH;
+    //    if ('/' == b[0] && '/' == b[1])
+    //        state = DOUBLE_SLASH;
 
-        if (state != DOUBLE_SLASH) {
-            printf("%s", b);
-        }
-    }
+    //    if (state != DOUBLE_SLASH) {
+    //        printf("%s", b);
+    //    }
+    //}
 }
 
 int getline(char s[])
@@ -38,4 +43,44 @@ int getline(char s[])
     s[i] = '\0';
 
     return i;
+}
+
+void code()
+{
+    char c = getchar();
+
+    if (c !=EOF) {
+        if ('/' == c)
+            slash(c);
+        else {
+            putchar(c);
+            code();
+        }
+    }
+
+}
+
+void slash(char prev)
+{
+    char curr = getchar();
+
+    if ('/' == curr)
+        double_slash();
+    else {
+        putchar(prev);
+        putchar(curr);
+        code();
+    }
+}
+
+void double_slash()
+{
+    char c = getchar();
+
+    if (c == '\n') {
+        putchar(c);
+        code();
+    }
+    else
+        double_slash();
 }

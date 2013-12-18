b163a30487e982b90b4bb112a61ff699675a35bd
Gideon Dresdner <gideond@gmail.com>
2013-12-17 19:13:56 -0500

    Old changes from the hard way.

---
+++ the-hard-way/ex4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/* Warning: this is a piece of trash. */
+
+int main()
+{
+    int age = 10;
+    int height = 60;
+
+    printf("I am %d years old.\n", age);
+    printf("I am %d inches tall.\n", height);
+
+    return 0;
+}

ae9cdc17d11311b539507288a43638d838fadb7b
Gideon Dresdner <gideond@gmail.com>
2014-11-14 23:16:13 -0500

    basic automated testing

---
+++ wc/test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+DATA_DIR="test-data"
+
+TEST_COUNT=0
+FAILED_COUNT=0
+for f in $(ls $DATA_DIR)
+do
+    ((TEST_COUNT++))
+
+    WC_COUNT=`wc $DATA_DIR/$f   | sed -e 's/[[:blank:]]//g'`
+    MY_COUNT=`./wc $DATA_DIR/$f | sed -e 's/[[:blank:]]//g'`
+
+    if [ "$WC_COUNT" != "$MY_COUNT" ]
+    then
+        echo $f "FAILED TEST"
+        ((FAILED_COUNT++))
+    fi
+done
+
+echo
+echo $TEST_COUNT tests run
+echo $FAILED_COUNT failed
